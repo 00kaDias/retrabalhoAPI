64161ec4a7d5757de84305f338ee5003d5724fa9
Kauan Victor <victtordiasp1@gmail.com>
2026-09-21 18:32:20 -0300

    Setup

---
+++ src/main/java/com/lumiere/retrabalhoapi/RetrabalhoapiApplication.java
@@ -0,0 +1,13 @@
+package com.lumiere.retrabalhoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrabalhoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrabalhoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lumiere/retrabalhoapi/RetrabalhoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumiere.retrabalhoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrabalhoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
